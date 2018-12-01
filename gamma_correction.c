9b2a579dabf9dd6ec2f755cf0cc63c713e0344a1
danyu_li <lidanyu0904@gmail.com>
2018-11-30 23:51:29 -0800

    add more comments

--- gamma_correction.c
+++ gamma_correction.c
@@ -1,16 +1,57 @@
+/****************************************************************************
+  This sample is released as public domain.  It is distributed in the hope it
+  will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  This is the sample code of applying gamma correction to a raw image in the 
+  format of YUV422 16-bit.The image capture is based on monochrome camera. 
+
+  Author: Danyu Li
+  Date: 11/30/18 
+*****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-#define IMAGE_WIDTH 1280
+/****************************************************************************
+**                     Global data * function declaration
+*****************************************************************************/
+//default testing resolution and raw image
+#define IMAGE_WIDTH 1280 
 #define IMAGE_HEIGHT 720
 #define FILE_NAME_IN "1mm_1.raw"
 #define FILE_NAME_OUT "gamma_corrected.raw"
 
 double gamma_val = 1/2.2;
+
 void gamma_correction_filter(unsigned short *img_in, unsigned short *img_out,
                                 int image_height, int image_width);
+/*****************************************************************************
+**                           Function definition
+*****************************************************************************/
+void gamma_correction_filter(unsigned short *img_in, unsigned short *img_out,
+                                int image_height, int image_width)
+{
+    unsigned short tmp;
+    unsigned char illuminance;
+    double illuminance_tmp, illumiance_gamma;
+
+    for (int i = 0; i < image_height; i++) {
+        for (int j = 0; j < image_width; j++) {
+            tmp = img_in[i*image_width + j];
+            illuminance = (tmp) & 0xff;
+            illuminance_tmp = (double)illuminance/256;
+            illumiance_gamma = pow(illuminance_tmp, gamma_val);
+            illumiance_gamma = illumiance_gamma * 256;
+            illuminance = (unsigned char)illumiance_gamma; 
+            tmp = ((0x80 <<8)|illuminance);
+            img_out[i*image_width+j] = tmp; 
+            
+        }
+    }
+
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -46,9 +87,11 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    img = (unsigned short *)malloc(image_width*image_height*sizeof(unsigned short));
+    img = (unsigned short *)malloc(image_width*image_height* \
+                sizeof(unsigned short));
     img_out = (unsigned short *)calloc(image_width*image_height,
                 sizeof(unsigned short));
+
     if (img == NULL) 
     {
         printf("malloc error for size = %ld\r\n", 
@@ -90,28 +133,3 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void gamma_correction_filter(unsigned short *img_in, unsigned short *img_out,
-                                int image_height, int image_width)
-{
-    unsigned short tmp;
-    unsigned char tmp2, illuminance;
-
-    double illuminance_tmp, illumiance_gamma;
-    for (int i = 0; i < image_height; i++) {
-        for (int j = 0; j < image_width; j++) {
-            tmp = img_in[i*image_width + j];
-            illuminance = (tmp) & 0xff;
-            illuminance_tmp = (double)illuminance/256;
-            illumiance_gamma = pow(illuminance_tmp, gamma_val);
-            illumiance_gamma = illumiance_gamma * 256;
-            //tmp2 = (unsigned char)illumiance_gamma; 
-            //tmp = ((0x80 <<8)|tmp2);
-            illuminance = (unsigned char)illumiance_gamma; 
-            tmp = ((0x80 <<8)|illuminance);
-            img_out[i*image_width+j] = tmp; 
-            
-        }
-    }
-
-
-}
